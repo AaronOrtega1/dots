c4b8f7676687a62f426651633dadeefac3b304e4
AaronOrtega1 <aaron.ortega15@outlook.es>
2024-12-05 18:08:45 -0600

    changed tab space nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-if vim.fn.exists("g:vscode") == 0 then
-  vim.lsp.buf.clear_references()
+if vim.g.vscode then
+  vim.o.cmdheight = 10
+  return
 end
