1042729af4d579c773a7dea5c521de8f2e9f59b7
AaronOrtega1 <aaron.ortega15@outlook.es>
2024-12-27 13:35:04 -0600

    changed init.lua to make it detect when nvim its used in vscode o not

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,8 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
 if vim.g.vscode then
+  vim.opt.clipboard = "unnamedplus"
   vim.o.cmdheight = 10
   return
+else
+  -- bootstrap lazy.nvim, LazyVim and your plugins
+  require("config.lazy")
 end
