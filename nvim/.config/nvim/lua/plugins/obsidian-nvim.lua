0dcbc92a2f8c8f89f0216180356a5a29a3419794
AaronOrtega1 <aaron.ortega15@outlook.es>
2025-01-01 14:26:25 -0600

    changed urgent in waybar and obsdian for nvim conf

--- nvim/.config/nvim/lua/plugins/obsidian-nvim.lua
+++ nvim/.config/nvim/lua/plugins/obsidian-nvim.lua
@@ -21,14 +21,55 @@ return {
     workspaces = {
       {
         name = "personal",
-        path = "~/vaults/personal",
+        path = "~/Documents/127.0.0.1/",
       },
       {
-        name = "work",
-        path = "~/vaults/work",
+        name = "test-nvim",
+        path = "~/Documents/test-nvim/",
       },
     },
 
-    -- see below for full list of options 👇
+    -- Completion
+    completion = {
+      -- Set to false to disable
+      nvim_cmp = true,
+
+      -- Trigger completion at 2 chars
+      min_chars = 2,
+    },
+
+    -- Templates
+    templates = {
+      folder = "5 - Templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+
+    daily_notes = {
+      -- Daily notes directory
+      folder = "6 - Daily Notes",
+      -- File name format
+      date_format = "%Y-%m-%d",
+      -- Template
+      template = "5 - Templates/Daily Note Template",
+    },
+
+    -- URL
+    ---@param url string
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      -- vim.fn.jobstart({"open", url})  -- Mac OS
+      vim.fn.jobstart({ "xdg-open", url }) -- linux
+      -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+      -- vim.ui.open(url) -- need Neovim 0.10.0+
+    end,
+
+    -- Specify how to handle attachments.
+    attachments = {
+      -- Folder where attachments are stored.
+      img_folder = "3 - Resources/Assets",
+    },
   },
 }
