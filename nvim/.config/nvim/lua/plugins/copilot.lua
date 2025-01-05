9b6c8a34ef3d3568cf772d0f2f4f9ed790ca5a3d
AaronOrtega1 <aaron.ortega15@outlook.es>
2025-01-05 09:23:58 -0600

    changed nvim conf and added wallpapers

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -4,6 +4,6 @@ return {
     -- Configuración opcional para Copilot
     vim.g.copilot_no_tab_map = true
     vim.api.nvim_set_keymap("i", "<A-l>", "copilot#Accept()", { expr = true, silent = true })
-    vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    -- vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
   end,
 }
