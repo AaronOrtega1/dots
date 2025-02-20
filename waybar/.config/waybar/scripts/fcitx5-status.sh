2de2a0d2b19b37375ef09a775432538c841d834d
AaronOrtega1 <aaron.ortega15@outlook.es>
2025-02-19 18:12:19 -0600

    changed waybar por laptop and script that gets current layer with fcitx5

--- waybar/.config/waybar/scripts/fcitx5-status.sh
+++ waybar/.config/waybar/scripts/fcitx5-status.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 # Obtiene el método de entrada actual desde Fcitx 5
-fcitx5-remote -n
+current_layout=$(fcitx5-remote -n)
+
+# Verifica si no hay ningún método de entrada activo
+if [ -z "$current_layout" ]; then
+  echo ""
+else
+  # Verifica si el método de entrada es japonés
+  if [[ "$current_layout" == *"mozc"* ]]; then
+    echo "あ"
+  elif [[ "$current_layout" == *"keyboard-us"* ]]; then
+    echo "en"
+  else
+    echo "$current_layout"
+  fi
+fi
+
