84f253b8960d2c5a35e196a627cdcfa4f1bb1f1a
AaronOrtega1 <aaron.ortega15@outlook.es>
2025-03-30 18:22:57 -0600

    nvim conf for vscode extension

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,8 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- init.lua
+local is_vscode = vim.fn.exists("g:vscode") == 1 or os.getenv("VSCODE_NVIM") ~= nil
+
+if not is_vscode then
+  require("config.lazy")
+else
+  require("config.vscode")
+end
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,5 +3,12 @@
 -- Add any additional keymaps here
 local keymap = vim.keymap
 local opts = { noremap = true }
+local opts_silent = { noremap = true }
 
 keymap.set("n", "<Leader>;", ":", { noremap = true, desc = "Enter Command Line" })
+
+-- Save all file with <C-s>
+keymap.set("n", "<C-s>", ":wa<Return>", { noremap = true, desc = "Save all files", silent = true })
+
+-- Toggle folding with za in normal mode
+keymap.set("n", "za", "za", opts)
