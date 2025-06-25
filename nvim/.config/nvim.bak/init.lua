c2b458b35d95bf24c19972fad090ea10963d3107
AaronOrtega1 <aaron.ortega15@outlook.es>
2025-06-25 08:47:22 -0600

    deleted back conf of nvim, changed waybar and hyprland, deleted unused scripts

--- nvim/.config/nvim.bak/init.lua
+++
@@ -1,8 +0,0 @@
--- init.lua
-local is_vscode = vim.fn.exists("g:vscode") == 1 or os.getenv("VSCODE_NVIM") ~= nil
-
-if not is_vscode then
-  require("config.lazy")
-else
-  require("config.vscode")
-end
--- nvim/.config/nvim.bak/lua/config/vscode.lua
+++
@@ -1,5 +0,0 @@
-local vscode = require("vscode-neovim")
-
-vim.g.mapleader = " "
--- Mejor manejo del portapapeles
-vim.opt.clipboard = "unnamedplus"
